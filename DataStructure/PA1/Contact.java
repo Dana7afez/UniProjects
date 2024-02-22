0b6e13eafc3ce253fe905b7a54a626662554eaa8
DH <156857033+2DH0@users.noreply.github.com>
2024-02-22 13:28:11 +0300

    Add files via upload

---
+++ DataStructure/PA1/Contact.java
@@ -0,0 +1,94 @@
+
+import java.util.Date;
+
+/*
+Contact: This class will represent a single contact in the phonebook. It should have fields for the 
+contact’s name, phone number, email address, address, birthday, and notes. */
+
+/**
+ *
+ * @author Manal Alhihi
+ */
+public class Contact implements Comparable<Contact> {
+    String name;
+    String phonenumber;
+    String emailaddress;
+    String address;
+    Date birthday; 
+    String notes;
+    LinkedList<Event> events ; 
+   
+    public Contact() {
+        this.name = "";
+        this.phonenumber = "";
+        this.emailaddress = "";
+        this.address = "";
+        this.birthday = null;
+        this.notes = "";
+        events = new LinkedList<Event>();
+    }
+
+    public Contact(String name, String phonenumber, String emailaddress, String address, String birthday, String notes) {
+        this.name = name;
+        this.phonenumber = phonenumber;
+        this.emailaddress = emailaddress;
+        this.address = address;
+        this.birthday = new Date(birthday);
+        this.notes = notes;
+        events = new LinkedList<Event>();
+    }
+
+    @Override
+    public String toString() {
+        return "\nName: " + name +
+                    "\nPhone Number: " + phonenumber +
+                    "\nEmail Address: " + emailaddress +
+                    "\nAddress: " +  address +
+                    "\nBirthday: " + birthday +
+                    "\nNotes: " + notes + "\n";
+    }
+
+    public boolean addEvent( Event e)
+    {
+        if (! events.empty())
+        {
+            events.findFirst();
+            for ( int i = 0 ; i < events.size ; i++)
+            {
+                if ((events.retrieve().date.compareTo(e.date) == 0) 
+                        && (events.retrieve().time.compareTo(e.time) == 0))
+                    return false;
+            }
+      }
+    events.insertSort(e);
+    return true;
+    }
+
+    public boolean removeEvent( String eTitle)
+    {
+        if (events.empty())
+            return false;
+       Event val = new Event();
+       val.title = eTitle;
+        if (events.search(val))
+        {
+            events.remove(val);
+            return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public int compareTo(Contact o) {
+        try {
+            return (this.name.compareTo(o.name));
+        }
+        catch (Exception e)
+        {
+             //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+
+
+}
---
+++ DataStructure/PA1/Event.java
@@ -0,0 +1,79 @@
+
+import java.util.Date;
+
+/*
+Event: This class will represent events or appointment that can be scheduled with a contact. It 
+should have fields for the event title, date and time, location, and the contact involved in this 
+event.
+*/
+
+/**
+ *
+ * @author Manal Alhihi
+ */
+public class Event implements Comparable<Event> {
+    String title;
+    Date date;
+    String time;
+    String location;
+    LinkedList <String> contacts_names;
+
+    public Event() {
+        this.title = "";
+        this.date = null;
+        this.time = "";
+        this.location = "";
+        this.contacts_names = new LinkedList<String> ();
+    }
+    
+    public Event(String title, String date, String time, String location, String contact) {
+        this.title = title;
+        this.date = new Date(date);
+        this.time = time;
+        this.location = location;
+        this.contacts_names = new LinkedList<String> ();
+        contacts_names.insertSort(contact);
+    }
+
+    public boolean addContact (String contact)
+    {
+        return contacts_names.insertSort(contact);
+    }
+    
+    public boolean removeContact(String contact)
+    {
+            String name = contacts_names.remove(contact);
+            if ( name != null)
+                return true; 
+            return false;
+    }
+
+    @Override
+    public String toString() {
+        String str = "\nEvent title: " + title +
+                    "\nEvent date and time (MM/DD/YYYY HH:MM): " + date + time +
+                   "\nEvent location: " + location + "\n" +
+                    "\nContacts names:   " ;
+                
+        contacts_names.findFirst();
+         for ( int i = 0 ; i < contacts_names.size ; i++ )
+         {
+             str += contacts_names.retrieve() + "\t";
+             contacts_names.findNext();
+         }
+          return str;
+    }
+
+    @Override
+    public int compareTo(Event obj) {
+        try {
+            return (this.title.compareToIgnoreCase(obj.title));
+        }
+        catch (Exception e)
+        {
+             //To change body of generated methods, choose Tools | Templates.
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    }
+
+}
