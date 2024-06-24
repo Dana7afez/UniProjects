864247166dbcc1c2847e9e0bc70f01d580ec5309
DanaHafez <156857033+2DH0@users.noreply.github.com>
2024-06-24 18:57:36 +0300

    Update Phonebook.java

--- DataStructure/PA1/Phonebook.java
+++ DataStructure/PA1/Phonebook.java
@@ -1,19 +1,12 @@
-
 import java.util.Date;
 import java.util.Scanner;
 
 /*
- This class will represent the phonebook application itself. It should have a field for 
-the linked list ADT that stores the contacts and methods for interacting with the list (e.g., adding, 
-searching, and deleting contacts). You will also need to schedule events and appointments with 
-contacts.
+This class will represent the phonebook application itself. It should have a field for the linked list ADT that stores the contacts and methods 
+for interacting with the list (e.g., adding, searching, and deleting contacts). You will also need to schedule events and appointments with contacts.
 */
 
-/**
- *
- * @author Manal Alhihi
- * 
- */
+
 public class Phonebook {
 
     /**
