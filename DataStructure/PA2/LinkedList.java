73bd72f80819f2a0cc0c1d971703a131d9a92557
DanaHafez <156857033+2DH0@users.noreply.github.com>
2024-06-24 18:59:27 +0300

    Update LinkedList.java

--- DataStructure/PA2/LinkedList.java
+++ DataStructure/PA2/LinkedList.java
@@ -1,13 +1,7 @@
 /**
- * LinkedList ADT: This class will represent the linked list data structure used to store the contacts. 
+ LinkedList ADT: This class will represent the linked list data structure used to store the contacts. 
  It should have methods for adding, searching, and deleting contacts from the list.
 **/
-
-/**
- *
- * @author Manal Alhihi
- * @param <T>
- */
 
 public class LinkedList<T extends Comparable<T>> {
     class Node<T>{
